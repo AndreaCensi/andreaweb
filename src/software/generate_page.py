5bc2c7d38d6a9bbd2e94a711e5bd2b926268eb1b
Andrea Censi <andrea@cds.caltech.edu>
2011-01-26 17:36:23 -0800

    updated

--- src/software/generate_page.py
+++ src/software/generate_page.py
@@ -1,15 +1,14 @@
-import YAML
+import yaml, shutil
 
 
 def main():
     definitions = 'software.yaml'
     groups = 'groups.yaml'
     head = 'software.page.head'
-    output = 'index.page'
-    output = 'index.html'
+    output = '../software.page'
     
-    projects = YAML.load_all(open(definitions))
-    groups = YAML.load_all(open(groups))
+    projects = list(yaml.load_all(open(definitions)))
+    groups = list(yaml.load_all(open(groups)))
     
     projects = [p for p in projects if not p.get('hide', False)]
     
@@ -18,10 +17,13 @@ def main():
         f.write(hf.read())
     render(groups, projects, f)
 
+    f.close()
+    
+    shutil.copyfile(output, output+'.html')
+    
 def render(groups, projects, f):
     
     project_groups = set([p['group'] for p in projects])
-    
     
     for g in groups:
         gp = [p for p in projects if p['group'] == g['id']]
@@ -30,34 +32,39 @@ def render(groups, projects, f):
         f.write(group_template.format(**d))
         
         f.write(table_head)
-        for p in gp:
+        for p in sorted(gp, key=lambda x: -x['date']):
             d=dict( name=p['name'],
                     active=string_active(p),
                     status=string_status(p),
                     language=string_language(p),
-                    desc_html=md2html(p))
-            f.write(row_template.format(**d))
+                    year = p['date'],
+                    url=p['url'],
+                    desc_html=md2html(p['desc']))
+            row =row_template.format(**d)
+            f.write(row)
         f.write(table_foot)
     
     all_group_id = [g['id'] for g in groups]
-    for p in projects:
-        if p['group'] not in all_group_id:
-            print('Warning: unknown group %r' % p['group'])
+    # print('Groups used by projects: %r' % project_groups)
+    # print('Groups declared: %r' % all_group_id)
+    for pg in project_groups:
+        if pg not in all_group_id:
+            print('Warning: unknown group %r' % pg)
     
-group_template = 
-"""
-<h2> {name} </h2>
-<p> {desc} </p>
+group_template = """
+<h2 class='group_name'> {name} </h2>
+<p class='group_desc'> {desc} </p>
         
 """
 table_head = """
 <table class='projects'>
 <tr class='header'>
-    <td>Name</td>
-    <td>Active</td>
-    <td>Status</td>
-    <td>Language</td>
-    <td>Description</td>
+    <td class='name'></td>
+    <td class='year'>started</td>
+    <td class='active'>active</td>
+    <td class='status'>status</td>
+    <td class='language'>language</td>
+    <td class='desc'></td>
 </tr>
 
 """
@@ -68,31 +75,61 @@ table_foot = """
 
 row_template = """
     <tr class='project'>
-        <td>{name}</td>
-        <td>{active}</td>
-        <td>{status}</td>
-        <td>{language}</td>
-        <td>{desc_html}</td>
+        <td class='name'><a href='{url}'>{name}</a></td>
+
+        <td class='year' >{year}</td>
+        <td class='active' >{active}</td>
+
+        <td class='status'>{status}</td>
+        <td class='language'>{language}</td>
+        <td class='desc'>{desc_html}</td>
     </tr>
 """
 def string_active(p):
-    options = dict(yes='yes',
-                    no='no',
-                    maintained='Maintained',
-                    others='By others')
-    return options[p.get('active')]
+    active = '<img class="activity_icon" src="icons/status/activity-active.png" alt="active"/>'
+    no = '<img class="activity_icon" src="icons/status/activity-notactive.png" alt="not active"/>'
+    maintained = '<img class="activity_icon" src="icons/status/activity-maintained.png" alt="maintained by me"/>'
+    others =  '<img class="activity_icon" src="icons/status/activity-others.png" alt="maintained by others"/>'
+    
+    options = { True: active,
+                False: no,
+                    'maintained': maintained,
+                    'others': others}
+    return get(p, 'active', options)
 
+def get(record, field, options):
+    rid = record.get('name', record)
+    if not field in record:
+        print('Record %r does not have %r.' % (rid, field))
+        return '!'
+    value = record[field]
+    if not value in options:
+        print('Record %r has strange choice %r for %r (I want %r) ' % (rid, value, field,options.keys()))
+        return '?'
+    return options[value]
+        
 def string_status(p):
-    options = dict(production='Stable',
-                    beta='Beta',
-                    alpha='Alpha')
-    return options[p.get('status')]
+    
+    options = dict(production='<img class="status_icon" src="icons/status/status-stable.png" alt="stable"/>',
+                    beta='<img class="status_icon" src="icons/status/status-beta.png" alt="beta"/>',
+                    alpha='<img class="status_icon" src="icons/status/status-alpha.png" alt="alpha"/>')
+    return get(p, 'status', options)
 
 def string_language(p):
-    return p['language']
-
+    language = p.get('language', None)
+    if language is None:
+        print('No language for %r' % p['name'])
+        return '?'
+    return language
+import markdown
 def md2html(s):
-    return s
-    
+    if s is None:
+        s = '?'
+    html = markdown.markdown(s)
+    if '[' in html:
+        print('Markdown did not convert everything:\n------\n%s\n-------\n%s\n-----' %
+                (s,html))
+        
+    return html.encode("utf-8")
 if __name__ == '__main__':
     main()
