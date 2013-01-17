0b24dff460611b2c1627f528e92618997e3ebbf5
Andrea Censi <andrea@cds.caltech.edu>
2013-01-17 06:26:34 +0000

    done

--- wordpress-themes/catitselfA/myheader.php
+++ wordpress-themes/catitselfA/myheader.php
@@ -5,8 +5,7 @@
 
 <?php } ?>
 
-
-
+<meta name="viewport" content="width=1024" />
 
 <link rel="me" type="text/html" 
       href="http://www.google.com/profiles/106839984596770449716" />
@@ -38,7 +37,7 @@
 <link rel="stylesheet" type="text/css"
    href="http://releases.flowplayer.org/5.2.0/skin/functional.css" />
 
-
+<!-- zoom -->
 <script src="/media/js/jquery.imageZoom.js"></script>
 <link rel="stylesheet" type="text/css"  href="/media/js/jquery.imageZoom.css"/>
 <script type="text/javascript">
@@ -46,6 +45,7 @@
   $(".imageZoom").imageZoom(); 
  });
 </script>
+
 <!-- additional fonts -->
 <!-- <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css' /> -->
 <!-- <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'> -->
@@ -53,6 +53,3 @@
 <!-- <link href='http://fonts.googleapis.com/css?family=Signika:400,600,300,700' rel='stylesheet' type='text/css'/> -->
 <!-- <link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,400,700' rel='stylesheet' type='text/css'/> -->
 <link href='http://fonts.googleapis.com/css?family=Noticia+Text:400,400italic,700,700italic' rel='stylesheet' type='text/css'/>
-
-
-
